3b89f3ac7a3b8f8d899443ca9c9b777d1be3eeeb
desuned0 <88514315+desuned@users.noreply.github.com>
2026-01-30 21:05:24 +0300

    changed configs import to __init__.py file

---
+++ config/__init__.py
@@ -0,0 +1,4 @@
+from .integrity_config import INTEGRITY_CONFIG
+from .cache_config import CACHE_CONFIG
+from .rbac_roles import RBAC_ROLES
+from .ueba_config import UEBA_CONFIG
--- patient_monitoring_system.py
+++ patient_monitoring_system.py
@@ -1262,5 +1262,4 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
     main()
